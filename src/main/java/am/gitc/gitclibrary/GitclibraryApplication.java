53d033c14979ef4de359450f7edc1d2491e044c8
zorik <zak.zorik20@gmail.com>
2016-03-07 22:17:31 +0400

    all

---
+++ src/main/java/am/gitc/gitclibrary/GitclibraryApplication.java
@@ -0,0 +1,12 @@
+package am.gitc.gitclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitclibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitclibraryApplication.class, args);
+	}
+}
---
+++ src/test/java/am/gitc/gitclibrary/GitclibraryApplicationTests.java
@@ -0,0 +1,18 @@
+package am.gitc.gitclibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GitclibraryApplication.class)
+@WebAppConfiguration
+public class GitclibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
