1a6ea5aec631e2dd2b3fd4b1e14a44c193099889
zorik <zak.zorik20@gmail.com>
2016-05-26 21:43:11 +0400

    change register form

--- common/src/main/java/am/gitc/common/model/entity/User.java
+++ common/src/main/java/am/gitc/common/model/entity/User.java
@@ -14,30 +14,26 @@ public class User {
     private Long id;
 
     @Column(name = "name")
+
     private String name;
 
     @Column(name = "surname")
+
     private String surname;
 
     @Column(name = "email")
+
     private String email;
 
     @Column(name = "password")
+
     private String password;
 
     @Column(name = "age")
-    private short age;
+    private Short age;
 
 
     private Integer genderId;
-
-//    @Column(name = "userStatus")
-//    private UserStatus userStatus;
-
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-//    Set<Book> books;
-//
-
 
     public Long getId() {
         return id;
@@ -79,11 +75,11 @@ public class User {
         this.password = password;
     }
 
-    public short getAge() {
+    public Short getAge() {
         return age;
     }
 
-    public void setAge(short age) {
+    public void setAge(Short age) {
         this.age = age;
     }
 
@@ -93,34 +89,5 @@ public class User {
 
     public void setGenderId(Integer genderId) {
         this.genderId = genderId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (age != user.age) return false;
-        if (id != null ? !id.equals(user.id) : user.id != null) return false;
-        if (name != null ? !name.equals(user.name) : user.name != null) return false;
-        if (surname != null ? !surname.equals(user.surname) : user.surname != null) return false;
-        if (email != null ? !email.equals(user.email) : user.email != null) return false;
-        if (password != null ? !password.equals(user.password) : user.password != null) return false;
-        return genderId != null ? genderId.equals(user.genderId) : user.genderId == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (surname != null ? surname.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-        result = 31 * result + (password != null ? password.hashCode() : 0);
-        result = 31 * result + (int) age;
-        result = 31 * result + (genderId != null ? genderId.hashCode() : 0);
-        return result;
     }
 }
--- web/src/main/java/am/gitc/web/controller/RegistrationController.java
+++ web/src/main/java/am/gitc/web/controller/RegistrationController.java
@@ -1,14 +1,14 @@
 package am.gitc.web.controller;
 
-import am.gitc.common.model.entity.Gender;
 import am.gitc.common.model.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.LinkedList;
-import java.util.List;
+import javax.validation.Valid;
 
 /**
  * Created by Zorik Zaqaryan on 15.05.2016.
@@ -21,14 +21,20 @@ public class RegistrationController {
     @RequestMapping(method = RequestMethod.GET)
     public String initPage(Model model) {
         model.addAttribute("userForm", getUser());
-        model.addAttribute("genderList", getGenderList());
+//        model.addAttribute("genderList", getGenderList());
         return "registration";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String registerUser(Model model, User user) {
+    public String registerUser(Model model, @Valid @ModelAttribute("userForm") User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("userForm", user);
+            return "registration";
+        }
         model.addAttribute("userForm", user);
 //        model.addAttribute("genderList", getGenderList());
+
+        model.addAttribute("userForm", user);
 
         return "registration";
     }
@@ -39,19 +45,19 @@ public class RegistrationController {
     }
 
 
-    public List<Gender> getGenderList() {
-        List<Gender> genders = new LinkedList<>();
-        //
-        Gender male = new Gender();
-        male.setId(1);
-        male.setType("MALE");
-        //
-        Gender female = new Gender();
-        male.setId(2);
-        male.setType("FEMALE");
-
-        genders.add(male);
-        genders.add(female);
-        return genders;
-    }
+//    public List<Gender> getGenderList() {
+//        List<Gender> genders = new LinkedList<>();
+//        //
+//        Gender male = new Gender();
+//        male.setId(1);
+//        male.setType("MALE");
+//        //
+//        Gender female = new Gender();
+//        male.setId(2);
+//        male.setType("FEMALE");
+//
+//        genders.add(male);
+//        genders.add(female);
+//        return genders;
+//    }
 }
